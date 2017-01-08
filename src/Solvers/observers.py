e8715095b2f520eb0dd65f9a38f3fc25aa27e303
EbbeS <stubbe.ebbe@gmail.com>
2017-01-08 13:31:23 +0100

    merged-observers
    Merged the observers used explicitly for the simplex, but maybe should
    use abstract classes?

--- src/Solvers/observers.py
+++ src/Solvers/observers.py
@@ -7,31 +7,7 @@ Created on Mon Dec 12 19:09:01 2016
 import numpy as np
 import timeit
 
-class observer_simplex_print_log():        
-    def notify_solve_start(self):
-        print("Started solving")
-    def notify_solve_end(self):
-        print("Ending solving")
-    def notify_reflecting(self):
-        print("Reflecting")    
-    def notify_expanding(self):
-        print("Expanding")
-    def notify_contracting(self):
-        print("Contracting")
-    def notify_shrinking(self):
-        print("Shrinking")        
-    def notify_step_start(self):
-        pass
-    def notify_step_end(self):
-        pass
-
-#The results are given in a the data format [step iteration][points//vals][simplex corner]
-#so [results[i][1][0] for i in range(len(results))] gives the best value in each iteration
-        
-class observer_simplex_step_log():    
-    def __init__(self,optimizer):    
-        self.result = []
-        self.optimizer = optimizer
+class observer(object):
     def notify_solve_start(self):
         pass
     def notify_solve_end(self):
@@ -44,8 +20,34 @@ class observer_simplex_step_log():
         pass
     def notify_shrinking(self):
         pass
-    def notify_step_start(self):
+    def notify_step_start(self):        
         pass
+    def notify_step_end(self):
+        pass
+    def get_result(self):
+        pass
+    
+class observer_simplex_print_log(observer):        
+    def notify_solve_start(self):
+        print("Started solving")
+    def notify_solve_end(self):
+        print("Ending solving")
+    def notify_reflecting(self):
+        print("Reflecting")    
+    def notify_expanding(self):
+        print("Expanding")
+    def notify_contracting(self):
+        print("Contracting")
+    def notify_shrinking(self):
+        print("Shrinking")        
+
+#The results are given in a the data format [step iteration][points//vals][simplex corner]
+#so [results[i][1][0] for i in range(len(results))] gives the best value in each iteration
+        
+class observer_simplex_step_log(observer):    
+    def __init__(self,optimizer):    
+        self.result = []
+        self.optimizer = optimizer
     def notify_step_end(self):
         points = self.optimizer.points
         vals = self.optimizer.values
@@ -53,7 +55,7 @@ class observer_simplex_step_log():
     def get_result(self):
         return self.result
 
-class observer_timeit():    
+class observer_timeit(observer):    
     def __init__(self):
         #The variables to be returned
         self.steptimes = []
@@ -68,21 +70,11 @@ class observer_timeit():
     def notify_solve_end(self):
         solve_endtime = timeit.default_timer()
         self.solvetime = solve_endtime - self.solve_starttime
-    def notify_reflecting(self):
-        pass    
-    def notify_expanding(self):
-        pass
-    def notify_contracting(self):
-        pass
-    def notify_shrinking(self):
-        pass
     def notify_step_start(self):
         self.step_starttime = timeit.default_timer()
     def notify_step_end(self):
         step_endtime = timeit.default_timer()
         self.steptimes.append(step_endtime - self.step_starttime)
-    def get_result(self):
-        pass
     def get_steptimes(self):
         return self.steptimes
     def get_solvetime(self):
