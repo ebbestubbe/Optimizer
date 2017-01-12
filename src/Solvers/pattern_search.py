3a3df434c614a7b114eeadd4b9f32dc337dcd8a0
EbbeS <stubbe.ebbe@gmail.com>
2017-01-12 17:15:29 +0100

    pattern-search
    implemented pattern search.
    
    still need proper termination strategy ( logging of # function
    evaluations)
    still need shrinking strategy
    
    Something causes the contours to fuck up, possibly shrinking of the
    alpha?, track it

---
+++ src/Solvers/pattern_search.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 12 15:14:49 2017
+
+@author: Ebbe
+"""
+from solver import solver_interface
+import numpy as np
+
+class pattern_search(solver_interface):
+    
+    def __init__(self, abs_tol = 0.0001, rel_tol = 0.0001, max_iter = 1000,start_size = 0.005,alpha_reduc_factor = 0.8):
+        super().__init__()
+        self.abs_tol = abs_tol
+        self.rel_tol = rel_tol
+        self.max_iter = max_iter
+        
+        self.id = "PATTERN_SEARCH"
+        #the size of the stepping algorithm, and the reduction param
+        self.size = start_size
+        
+
+    def step_alg(self):
+        point_candidates = []
+        value_candidates = []
+        for i in range(self.func.n_dim):
+            point_mod = np.zeros(self.func.n_dim)
+            point_mod[i] = self.size
+            
+            point_neg = np.clip(self.points - point_mod, self.func.bounds[0], self.func.bounds[1])
+            value_neg = self.func.evaluate(point_neg)
+            
+            point_pos = np.clip(self.points + point_mod, self.func.bounds[0], self.func.bounds[1])
+            value_pos = self.func.evaluate(point_pos)
+            
+            point_candidates.append([point_neg, point_pos])
+            value_candidates.append([value_neg, value_pos])
+        #min in each direction:
+        #for each dimension, 0 for negative, 1 for positive
+        min_direction = [x.index(min(x)) for x in value_candidates] 
+        #The minimum value in each dimension
+        min_values = [value_candidates[i][min_direction[i]] for i in range(self.func.n_dim)]
+        #The dimension with the lowest func value
+        min_dimension = min_values.index(min(min_values))
+        #The coordinates to switch to:
+        min_coords = [min_dimension,min_direction[min_dimension]]
+        
+        val_lowest = value_candidates[min_coords[0]][min_coords[1]]
+        if(val_lowest < self.values):
+            self.values = val_lowest
+            self.points = point_candidates[min_coords[0]][min_coords[1]]
+        else:
+            self.size*=0.5
+
+    def solve_alg(self, func, point_start):
+        self.func = func
+        self.points = point_start
+        self.values = self.func.evaluate(self.points)
+        
+        n_iter = 0
+        while(n_iter < self.max_iter):
+            n_iter+=1   
+            
+            self.step()
+            
+        return([self.values,self.points])
--- src/run_tests.py
+++ src/run_tests.py
@@ -9,11 +9,26 @@ import matplotlib.pyplot as plt
 
 from Solvers.simplex import simplex
 from Solvers.naive_line_search import naive_line_search
+from Solvers.pattern_search import pattern_search
 import Solvers.observers
 import Functions.test_functions
 from testing_methods import fullreport
 
 def main():
+    '''
+    c = [[0,-8],[-2,6],[3,-9]]
+    
+    b = [x.index(min(x)) for x in c]
+    d = [c[i][b[i]] for i in range(len(b))]
+    print(b)
+    print(d)
+    minval_ind = d.index(min(d))
+    print(minval_ind)
+    minval_coord = [minval_ind, b[minval_ind]]
+    print(minval_coord)
+    print(c[minval_coord[0]][minval_coord[1]])
+    return
+    '''
     plt.close("all")
     
     rel_tol = 10e-10
@@ -21,9 +36,9 @@ def main():
     rel_tol = 0
     abs_tol = 0
     
-    max_iter = 200
-    start_size = 0.005
-    
+    max_iter = 2000
+    start_size = 0.5
+    '''
     solver1 = simplex(rel_tol = rel_tol, abs_tol = abs_tol, max_iter = max_iter,start_size = start_size)
     
     #fullreport(solver1)
@@ -36,6 +51,11 @@ def main():
     solver2 = naive_line_search(rel_tol = rel_tol, abs_tol = abs_tol, max_iter = max_iter,start_size = start_size,alpha_reduc_factor = alpha_reduc_factor)
     
     fullreport(solver2)
+    '''
+    solver3 = pattern_search(rel_tol = rel_tol, abs_tol = abs_tol, max_iter = max_iter,start_size = start_size)
+    
+    fullreport(solver3)
+   
     
 if __name__ == '__main__':
     main()
